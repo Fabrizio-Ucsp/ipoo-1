15957462f5c847e461516c623856e6535d3ab34e
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-12-02 19:23:28 -0500

    Update main.cpp

--- game/main.cpp
+++ game/main.cpp
@@ -6,55 +6,64 @@
 #include "bala.cpp"
 using namespace std;
 int main(){
-    hidecursor();
-    navep n1(2,4,3,3);
-    int score=0,masdif=200;
-    vector<asteroide*> astgroup;
-    for(int i=0;i<10;i++){astgroup.push_back(new asteroide(rand()%39+60,rand()%31+4));}
-    vector<bala*> B;
-    bool game_over=false;
-    limites();
-    n1.impnav();
-    n1.corazones();
-    while(!game_over){
-        gotox(0,0);cout<<"Score  :"<<score;score++;
-        if(kbhit()){
-            char tecla=getch();
-            n1.mover(tecla);
-            if(tecla=='j'){B.push_back(new bala(n1.X()+7,n1.Y()+4));}
-        }
-        for(int it=0;it<B.size();it++){
-            (*B[it]).mover();
-            if((*B[it]).fuera()){
-                gotox((*B[it]).X(),(*B[it]).Y());cout<<" ";
-                delete(B[it]);
-                B.erase(B.begin()+it);
-            }
-        }
-        for(int it=0;it<astgroup.size();it++){
-            (*astgroup[it]).mover();
-            (*astgroup[it]).colision(n1);
-        }
-        for(int itast=0;itast<astgroup.size();itast++){
-            for(int itbal=0;itbal<B.size();itbal++){
-                if((*astgroup[itast]).Y()==(*B[itbal]).Y()){
-                    if((*astgroup[itast]).X()==(*B[itbal]).X() or (*astgroup[itast]).X()==(*B[itbal]).X()+1){
-                        gotox((*B[itbal]).X(),(*B[itbal]).Y());cout<<" ";
-                        delete(B[itbal]);
-                        B.erase(B.begin()+itbal);
-                        gotox((*astgroup[itast]).X(),(*astgroup[itast]).Y());cout<<" ";
-                        (*astgroup[itast]).reload();
-                        score+=10;
-                    }
-                }
-            }
-        }
-        if(score>masdif){astgroup.push_back(new asteroide(99,rand()%31+4));masdif+=200;}
-        n1.muerte();
-        if(n1.gameend()){game_over=true;}
-        Sleep(30);
-    }
-    gameendtxt(score);
-    Sleep(5000);
-    return 0;
+	printname();
+	system("pause");
+	system("cls");
+	hidecursor();
+	navep n1(2,4,3,3);
+	int score=0,masdif=200;
+	vector<asteroide*> astgroup;
+	for(int i=0;i<10;i++){astgroup.push_back(new asteroide(rand()%39+60,rand()%31+4));}
+	vector<bala*> B;
+	bool game_over=false;
+	limites();
+	n1.impnav();
+	n1.corazones();
+	while(!game_over){
+		gotox(0,0);cout<<"Score  :"<<score;score++;
+		if(kbhit()){
+			char tecla=getch();
+			n1.mover(tecla);
+			if(tecla=='j'){B.push_back(new bala(n1.X()+7,n1.Y()+4));}
+		}
+		for(int it=0;it<B.size();it++){
+			(*B[it]).mover();
+			if((*B[it]).fuera()){
+				gotox((*B[it]).X(),(*B[it]).Y());cout<<" ";
+				delete(B[it]);
+				B.erase(B.begin()+it);
+			}
+		}
+		for(int it=0;it<astgroup.size();it++){
+			(*astgroup[it]).mover();
+			(*astgroup[it]).colision(n1);
+		}
+		for(int itast=0;itast<astgroup.size();itast++){
+			for(int itbal=0;itbal<B.size();itbal++){
+				if((*astgroup[itast]).Y()==(*B[itbal]).Y()){
+					if((*astgroup[itast]).X()==(*B[itbal]).X() or (*astgroup[itast]).X()==(*B[itbal]).X()+1){
+						gotox((*B[itbal]).X(),(*B[itbal]).Y());cout<<" ";
+						delete(B[itbal]);
+						B.erase(B.begin()+itbal);
+						gotox((*astgroup[itast]).X(),(*astgroup[itast]).Y());cout<<" ";
+						(*astgroup[itast]).reload();
+						score+=10;
+					}
+				}
+			}
+		}
+		if(score>masdif){astgroup.push_back(new asteroide(99,rand()%31+4));masdif+=200;}
+		n1.muerte();
+		if(n1.gameend()){game_over=true;}
+		Sleep(30);
+	}
+	Sleep(500);
+	system("cls");
+	gameendtxt(score);
+	gotox(12,32);system("pause");
+	system("cls");
+	gracias();
+	system("pause");
+	return 0;
 }
+
